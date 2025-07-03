5d95a0ee2ece3e44b71cf8c7efd5a5dd1190ece0
beyyzzc <beyyzzc@gmail.com>
2025-07-03 10:32:54 +0300

    Task 3 Stream API

---
+++ src/StreamOperations.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.util.stream.*;
+
+public class StreamOperations {
+    public static void main(String[] stmts){
+        List<Integer> numbers = IntStream.rangeClosed(1,100).boxed().collect(Collectors.toList());
+         int max = numbers.stream().max(Integer::compare).get();
+         int min = numbers.stream().min(Integer::compare).get();
+         System.out.println("Max: " + max + ", Min: " + min);
+
+
+        long listDivBy2and4 = numbers.stream().filter(n-> n%2 == 0 && n%4 == 0).count();
+        System.out.println("Divisible by 2 and 4:  " + listDivBy2and4);
+
+        long totalEven = numbers.stream().filter(n-> n%2 == 0).count();
+        long sumEven = numbers.stream().filter(n->n%2 == 0).mapToInt(Integer::intValue).sum();
+        System.out.println("Even Count: " + totalEven + ", Even Sum: " + sumEven);
+
+        long totalOdd = numbers.stream().filter(n-> n%2 != 0).count();
+        long sumOdd = numbers.stream().filter(n->n%2 != 0).mapToInt(Integer:: intValue).sum();
+        System.out.println("Odd Count: " + totalOdd + ", Odd Sum: " + sumOdd);
+
+        List<String> names = List.of("Jackson", "Edward", "Allie","Sam", "Edie");
+        long startsWithE = names.stream().filter(name -> name.startsWith("E")).count();
+        System.out.println("Names starting with E: " + startsWithE);
+    }
+}
